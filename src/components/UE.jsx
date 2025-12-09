2aae3de9cfd8cb936457ff77463fc3d74d80518f
keshavnagar <keshavnagar887@gmail.com>
2025-12-09 22:13:06 +0530

    set the state in useEffect

--- src/components/UE.jsx
+++ src/components/UE.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 const UE = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    // side effect code
-    console.log(`counter value is : ${count}`);
-    return () => {};
-  }, [
-    /*dependency*/
-    count,
-  ]);
+  const [date, setDate] = useState(0);
+
+  useEffect(
+    () => {
+      const updatedDate = new Date();
+      setDate(updatedDate.toLocaleTimeString())
+      // side effect code
+
+      return () => {};
+    },
+    [
+      /*dependency*/
+    ]
+  );
   return (
     <>
       <h1>Hello UseEffect</h1>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Date: {date}</h1>
     </>
   );
 };
