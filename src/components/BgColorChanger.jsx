252a8199c46b186ddbe0be978253be05782d3843
keshavnagar <keshavnagar887@gmail.com>
2025-12-16 22:05:39 +0530

    complete the raendom color generator by react state

--- src/components/BgColorChanger.jsx
+++ src/components/BgColorChanger.jsx
@@ -1,8 +1,26 @@
-const BgColorChanger = () => {
-    return(
-        <section className="bg-[#DD6031] h-screen w-screen">
+import { useState } from "react";
 
-        </section>
-    )
-}
-export default BgColorChanger
+const BgColorChanger = () => {
+  const [color, setColor] = useState("#DDDF31");
+  return (
+    <section
+      style={{ backgroundColor: color }}
+      className={`h-screen w-screen flex items-center justify-center `}
+    >
+      <button
+        className="bg-[#311E10] text-[#ECE4B7] p-4 text-[2rem]"
+        onClick={() => {
+          const randomColor =
+            `#` +
+            Math.floor(Math.random() * 16777215)
+              .toString(16)
+              .padStart(6, "0");
+          setColor(randomColor);
+        }}
+      >
+        Change Colour
+      </button>
+    </section>
+  );
+};
+export default BgColorChanger;
