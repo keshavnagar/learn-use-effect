f12d5884aa72622a7d42d45f524028c3ee355896
keshavnagar <keshavnagar887@gmail.com>
2025-12-16 22:17:33 +0530

    add the use effect , just put the whoole code in use effect callback function

--- src/components/BgColorChanger.jsx
+++ src/components/BgColorChanger.jsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BgColorChanger = () => {
   const [color, setColor] = useState("#DDDF31");
+  useEffect(() => {
+    const id = setInterval(() => {
+      const randomColor =
+        `#` +
+        Math.floor(Math.random() * 16777215)
+          .toString(16)
+          .padStart(6, "0");
+      setColor(randomColor);
+    }, 2000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
   return (
     <section
       style={{ backgroundColor: color }}
       className={`h-screen w-screen flex flex-col items-center justify-center  gap-2`}
     >
-      <button
-        className="bg-[#311E10] text-[#ECE4B7] p-4 text-[2rem]"
-        onClick={() => {
-          const randomColor =
-            `#` +
-            Math.floor(Math.random() * 16777215)
-              .toString(16)
-              .padStart(6, "0");
-          setColor(randomColor);
-        }}
-      >
-        Change Colour
-      </button>
-      <h1 className="bg-[#123412] text-[#EABE7C] p-4 text-[2rem]">Color code is : {color}</h1>
+      <h1 className="bg-[#123412] text-[#EABE7C] p-4 text-[2rem]">
+        Color code is : {color}
+      </h1>
     </section>
   );
 };
