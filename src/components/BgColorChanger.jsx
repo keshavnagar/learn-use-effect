cc2542dd7bc32455012937f171b20d951a056fd5
keshavnagar <keshavnagar887@gmail.com>
2025-12-16 22:10:26 +0530

    add the color code ui

--- src/components/BgColorChanger.jsx
+++ src/components/BgColorChanger.jsx
@@ -5,7 +5,7 @@ const BgColorChanger = () => {
   return (
     <section
       style={{ backgroundColor: color }}
-      className={`h-screen w-screen flex items-center justify-center `}
+      className={`h-screen w-screen flex flex-col items-center justify-center  gap-2`}
     >
       <button
         className="bg-[#311E10] text-[#ECE4B7] p-4 text-[2rem]"
@@ -20,6 +20,7 @@ const BgColorChanger = () => {
       >
         Change Colour
       </button>
+      <h1 className="bg-[#123412] text-[#EABE7C] p-4 text-[2rem]">Color code is : {color}</h1>
     </section>
   );
 };
