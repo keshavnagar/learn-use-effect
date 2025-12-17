3a4687102ed1f6b7856c0a557bffabe01892514c
keshavnagar <keshavnagar887@gmail.com>
2025-12-17 22:53:09 +0530

    add the logic to hide the reset button when time is 0

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -16,12 +16,36 @@ const Timer = () => {
   return (
     <section className="flex flex-col justify-center items-center h-screen w-screen bg-[#1f96028a]">
       <h1>{time}</h1>
-      <button
-        className={`${toggle ? `bg-red-300` : `bg-green-300`} p-4` }
-        onClick={() => setToggle(!toggle)}
-      >
-        {toggle ? "pause" : "resume"}
-      </button>
+      {time !== 0 && (
+        <button
+          className={`${toggle ? `bg-red-300` : `bg-green-300`} p-4 shadow-2xl`}
+          onClick={() => setToggle(!toggle)}
+        >
+          {toggle ? "pause" : "resume"}
+        </button>
+      )}
+      {time !== 0 && (
+        <button
+          onClick={() => {
+            setTime(0);
+            setToggle(false);
+          }}
+          className="bg-red-300 m-2 p-4"
+        >
+          Reset
+        </button>
+      )}
+      {time === 0 && (
+        <button
+          className="bg-green-300 m-2 p-4"
+          onClick={() => {
+            setTime(0);
+            setToggle(true);
+          }}
+        >
+          start
+        </button>
+      )}
     </section>
   );
 };
